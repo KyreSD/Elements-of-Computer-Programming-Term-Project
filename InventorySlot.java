9b339db0a8658fda90cf7f939dbd39f0983f86d7
MaloneyT575 <maloneytutu@gmail.com>
2025-03-13 17:09:33 -0400

    Update InventorySlot.java

--- InventorySlot.java
+++ InventorySlot.java
@@ -12,6 +12,10 @@ public class InventorySlot extends Actor
      * Act - do whatever the in wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public InventorySlot(){
+        setImage("inventory slot.png");
+        scale(50,50);
+    }
     public void act()
     {
         // Add your action code here.

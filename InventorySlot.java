e4a9688c7218f701ef8439a3822f446996ebd901
MaloneyT575 <maloneytutu@gmail.com>
2025-03-13 21:42:31 -0400

    inventory
    Starting to implement if a number is pressed a weapon spawns in the inventory slot

--- InventorySlot.java
+++ InventorySlot.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class InventorySlot extends Actor
 {
+    public Actor currentWeapon;
     /**
      * Act - do whatever the in wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,9 +16,19 @@ public class InventorySlot extends Actor
     public InventorySlot(){
         setImage("inventory slot.png");
         getImage().scale(65,65);
+        currentWeapon = null;
     }
+    
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.isKeyDown("1")){
+            Sword sword = new Sword();
+            getWorld().addObject(sword,300,375);
+            currentWeapon = sword;
+        } else if (Greenfoot. isKeyDown("2")){
+            Enemy enemy = new Enemy();
+            getWorld().addObject(enemy, 300, 375);
+            currentWeapon = enemy;
+        }
     }
 }

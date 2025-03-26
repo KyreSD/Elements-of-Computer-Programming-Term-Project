e01ef014a82ea3a696a2ad22c63af31ec1d617c0
MaloneyT575 <maloneytutu@gmail.com>
2025-03-25 21:15:42 -0400

    character selection
    created a new world to choose a character you want to play with

---
+++ CharacterSelection.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CharacterSelection here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CharacterSelection extends World
+{
+    public static int SELECT = 0;
+    /**
+     * Constructor for objects of class CharacterSelection.
+     * 
+     */
+    public CharacterSelection()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        Greenfoot.start();
+        addObject(new Placeholder("FullSheetBoy.png",1,1), 300, 200);
+        addObject(new Link(), 250 , 200);
+    }
+    public static void pickCharacter(){
+        if(Greenfoot.isKeyDown("right")){
+            SELECT = 1;
+        }
+        if(Greenfoot.isKeyDown("left")){
+            SELECT = 0;
+        }
+    }
+}

a394e92cf066dca02553d89e742d23e970211d26
MaloneyT575 <maloneytutu@gmail.com>
2025-04-16 23:54:50 -0400

    GameOver
    Created game over world, with a restartbutton image

---
+++ RestartButton.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class RestartButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RestartButton extends Actor
+{
+    /**
+     * Act - do whatever the RestartButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public RestartButton(){
+        GreenfootImage background = new GreenfootImage("RestartButton.png");
+        background.scale(130, 130);
+        setImage(background);
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}

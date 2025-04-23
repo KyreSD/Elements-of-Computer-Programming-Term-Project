bdaa560ced18cbda22b90f72dc0847ac22678234
Leandro (Lee) <leandrogomez01@icloud.com>
2025-04-22 20:43:55 -0400

    Update Dungeon1.java

--- Dungeon1.java
+++ Dungeon1.java
@@ -1,36 +1,36 @@
+// Dungeon1.java
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class World2 here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * First dungeon area that connects to other worlds.
  */
 public class Dungeon1 extends SuperWorld
 {
     /**
-     * Constructor for objects of class World2.
-     * 
+     * Constructor for objects of class Dungeon1.
      */
     public Dungeon1(PlayerOne player, int x, int y)
     {   
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        this.player = player;
-        addObject(player, x, y);
+        super(player, x, y);
+        
+        // Add world-specific objects here
     }
-    public void act(){
-        if(player.getX() > getWidth()-2){
-        WorldGrass7 next = new WorldGrass7(player, getWidth()-player.getX()+1,player.getY());
-        Greenfoot.setWorld(next);
-    }else if(player.getX() < 2){
-        Dungeon4 next = new Dungeon4(player, getWidth()-player.getX()-2,player.getY());
-        Greenfoot.setWorld(next);
-    }else if(player.getY() < 2){
-        WorldMain next = new WorldMain(player, player.getX(),getHeight()-player.getY()-2);
-        Greenfoot.setWorld(next);
-    }else if(player.getY() > getHeight()-2){
-        Dungeon2 next = new Dungeon2(player, player.getX(),getHeight()-player.getY()+2);
-        Greenfoot.setWorld(next);
-    }
+    
+    public void act() {
+        super.act(); // Call parent act to maintain UI and game logic
+        
+        if(player.getX() > getWidth() - 5) {
+            WorldGrass7 next = new WorldGrass7(player, 10, player.getY());
+            Greenfoot.setWorld(next);
+        } else if(player.getX() < 5) {
+            Dungeon4 next = new Dungeon4(player, getWidth() - 10, player.getY());
+            Greenfoot.setWorld(next);
+        } else if(player.getY() < 5) {
+            WorldMain next = new WorldMain(player, player.getX(), getHeight() - 10);
+            Greenfoot.setWorld(next);
+        } else if(player.getY() > getHeight() - 5) {
+            Dungeon2 next = new Dungeon2(player, player.getX(), 10);
+            Greenfoot.setWorld(next);
+        }
     }
 }

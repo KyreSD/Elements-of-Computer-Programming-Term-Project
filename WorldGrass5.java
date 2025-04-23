c45083723ac868830e3f4f5853f6d9dee6b25c27
Leandro (Lee) <leandrogomez01@icloud.com>
2025-04-22 20:47:22 -0400

    Update WorldGrass5.java

--- WorldGrass5.java
+++ WorldGrass5.java
@@ -1,33 +1,33 @@
+// WorldGrass5.java
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class World2 here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Fifth grass area world that connects to other worlds.
  */
 public class WorldGrass5 extends SuperWorld
 {
     /**
-     * Constructor for objects of class World2.
-     * 
+     * Constructor for objects of class WorldGrass5.
      */
     public WorldGrass5(PlayerOne player, int x, int y)
     {   
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        this.player = player;
-        addObject(player, x, y);
+        super(player, x, y);
+        
+        // Add world-specific objects here
     }
-    public void act(){
-    if(player.getX() < 2){
-        WorldGrass4 next = new WorldGrass4(player, getWidth()-player.getX()-2,player.getY());
-        Greenfoot.setWorld(next);
-    }else if(player.getY() > getHeight()-2){
-        WorldGrass2 next = new WorldGrass2(player, player.getX(),getHeight()-player.getY()+2);
-        Greenfoot.setWorld(next);
-    }else if(player.getX() > getWidth()-2){
-        WorldGrass6 next = new WorldGrass6(player, getWidth()-player.getX()+1,player.getY());
-        Greenfoot.setWorld(next);
+    
+    public void act() {
+        super.act(); // Call parent act to maintain UI and game logic
+        
+        if(player.getX() < 5) {
+            WorldGrass4 next = new WorldGrass4(player, getWidth() - 10, player.getY());
+            Greenfoot.setWorld(next);
+        } else if(player.getY() > getHeight() - 5) {
+            WorldGrass2 next = new WorldGrass2(player, player.getX(), 10);
+            Greenfoot.setWorld(next);
+        } else if(player.getX() > getWidth() - 5) {
+            WorldGrass6 next = new WorldGrass6(player, 10, player.getY());
+            Greenfoot.setWorld(next);
+        }
     }
-}
 }
